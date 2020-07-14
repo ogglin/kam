ba1085cf51b1ac77781d507cd157fa66a6b47f89
Server <server.ares@gmail.com>
2020-07-14 19:11:30 +0300

    init v0.1

--- app/views.py
+++ app/views.py
@@ -16,8 +16,9 @@ def shell_cmd(cmd, *kwargs):
     if '-L' in kwargs:
         proc = subprocess.check_output([cmd, kwargs[0]])
     else:
-        print(cmd, kwargs[0], kwargs[1], kwargs[2])
-        subprocess.run([cmd, kwargs[0], kwargs[1], kwargs[2]])
+        cm = cmd + kwargs[0] + kwargs[1] + kwargs[2]
+        print(cm)
+        subprocess.run(cm, shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
         proc = subprocess.check_output(['iptables', '-L'])
     # proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     # o, e = proc.communicate()
